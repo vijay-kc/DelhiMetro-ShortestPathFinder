b77b6d7547f9ffbf1276f84c02b3ca4a5bb17b74
vijay kumar chaurasiya <118093154+vijay-kc@users.noreply.github.com>
2023-10-19 16:28:17 +0530

    Add files via upload

---
+++ login.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int sum(int a,int b){
+    return a+b;
+}
+int main(){
+    int a=2,b=8;
+    printf("WELCOME TO DELHI METRO");
+   
+ 
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<login.h>
+int main(){
+    int a=2,b=3;
+     int x=sum(a,b);
+  printf("%d",x);
+}
