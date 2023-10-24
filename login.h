b8f0b6ad8b879ca000d6c7cce0834c09d6ed0f20
vijay kumar chaurasiya <118093154+vijay-kc@users.noreply.github.com>
2023-10-24 18:07:22 +0530

    Update login.h

--- login.h
+++ login.h
@@ -1,10 +1,25 @@
 #include<stdio.h>
-int sum(int a,int b){
-    return a+b;
-}
-int main(){
-    int a=2,b=8;
-    printf("WELCOME TO DELHI METRO");
-   
- 
+#include<stdlib.h>
+#include "userdata.h"
+int login(){
+    char username;
+    int userpassword;
+    // login page
+    printf("\t\t\t\t\t\tLOGIN\n");
+    printf("ENTER YOUR NAME:");
+    scanf(" % c\n",&username);
+    printf("ENTER YOUR PASSWORD(must be integer):");
+    scanf("%d\n",&userpassword);
+    int x= verify(start, username,userpassword);
+    switch (x)
+    {
+    case 0:
+        printf("INVALID INPUT");
+        break;
+    
+    case 1:
+        return 1;
+        break;
+    }
+
 }
